f21b6411c595554143b5c2d7e7f678310e63d334
Kishan Jasani <kishanjasani007@yahoo.in>
2026-01-17 23:47:24 +0530

    Fix author domain

--- coreline.php
+++ coreline.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * Tested up to: 6.9
  * Author: Kishan Jasani
- * Author URI: https://kishanjasani.com
+ * Author URI: https://kishanjasani.in
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: coreline
